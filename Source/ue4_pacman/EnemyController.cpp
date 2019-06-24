c281020683d619bece96eb50326d1eeb35be15f1
gsteigert <steigert@gmail.com>
2019-06-24 20:01:43 -0300

    Remove some unused enemy classes

--- Source/ue4_pacman/EnemyController.cpp
+++
@@ -1,9 +0,0 @@
-#include "EnemyController.h"
-#include "Engine/World.h"
-#include "GameFramework/PlayerController.h"
-#include "DrawDebugHelpers.h"
-#include "EnemyPawn.h"
-
-void AEnemyController::Tick(float deltaTime)
-{
-}
--- Source/ue4_pacman/EnemyPawnMovement.cpp
+++
@@ -1,2 +0,0 @@
-#include "EnemyPawnMovement.h"
-
